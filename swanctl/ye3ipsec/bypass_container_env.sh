245d3f36c2d91bd9ba3272f1bdfdbaadebb5e6da
palw3ey <113945369+palw3ey@users.noreply.github.com>
2024-11-28 22:09:18 -0300

    Update bypass_container_env.sh

--- swanctl/ye3ipsec/bypass_container_env.sh
+++ swanctl/ye3ipsec/bypass_container_env.sh
@@ -1,5 +1,5 @@
 
-## This file don't apply if dumb-init is used in the Dockerfile. You must use this : ENTRYPOINT ["sh", "--login", "-c", "/entrypoint.sh"]
+## This file doesn't apply if dumb-init is used in the Dockerfile. You must use this : ENTRYPOINT ["sh", "--login", "-c", "/entrypoint.sh"]
 
 ## These values will override container env variables, and used by entrypoint.sh on every restart. To activate and customize the configurations wanted, remove one or more # sign.
 
